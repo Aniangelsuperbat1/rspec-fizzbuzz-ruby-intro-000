b79033d7f83dc4849aef0c2b1b7f03bd38273cbd
Patrick Zhen <zhen.patrick@gmail.com>
2020-12-28 18:02:33 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,12 +2,11 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
-    puts "fizzbuzz"
+    puts "FizzBuzz"
   elsif int % 5 == 0
-    puts "buzz"
+    puts "Buzz"
   elsif int % 3 == 0
-    puts "fizz"
+    puts "Fizz"
   else
     puts nil
-
 end
